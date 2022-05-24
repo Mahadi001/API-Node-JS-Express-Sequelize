f512e6a2994b46ceff41f35ed5d8a810b4192ddc
Mohammed Mahadi <mohammed.mahadi@technuf.com>
2022-05-24 19:55:33 +0600

    model added

--- app.js
+++ app.js
@@ -3,11 +3,48 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
+const {Sequelize} = require('sequelize');
+
+// Connect database
+const db = new Sequelize('playlists', 'postgres', '123456', {
+    host: 'localhost',
+    dialect:  'postgres'
+  });
+
 const app = express();
+
+// Test database connection
+db.authenticate()
+  .then(() => console.log('Database connected successfully...'))
+  .catch(err => console.log('Error: '+err))
+
+const Playlist = db.define('playlist', {
+  id:{
+    field: 'PlaylistId',
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  name:{
+    field: 'Name',
+    type: Sequelize.STRING
+  }
+},
+{
+  timestamps: false
+});
+
 
 app.get('/', (req,res) => {
     res.send('Hello!')
 })
 
-app.listen(5000);
+app.get('/api/playlists/', (req,res) => {
+    Playlist.findAll()
+            .then(playlists => res.json(playlists))
+            .catch(err => console.log('Error: '+err))
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
 
